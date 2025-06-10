70669885d1bf74508ab7f43246af18824f03ab71
QQB-Roy <qqb137158189137@163.com>
2025-06-10 11:23:58 +0800

    rebuild

--- gradlew.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set GRADLE_WRAPPER_PROPERTIES=gradle\wrapper\gradle-wrapper.properties
-if not exist %GRADLE_WRAPPER_PROPERTIES% (
-    echo Wrapper properties not found. Please make sure gradle-wrapper.properties is present.
-    exit /b 1
-)
-java -jar gradle\wrapper\gradle-wrapper.jar %*
---
+++ src/main/java/com/example/servernoticeboard/ServerNoticeBoard.java
@@ -0,0 +1,11 @@
+package com.example.servernoticeboard;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ServerNoticeBoard implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("[ServerNoticeBoard] Initialized.");
+        // Register commands and load config here
+    }
+}
